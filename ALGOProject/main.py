b336d2eacb11fd13d14900751f0d9f8bfb7aeb8d
Huzaifa Abid <huzaifaa926@gmail.com>
2019-11-18 19:28:05 +0500

    Added helper files for each algo

--- ALGOProject/main.py
+++ ALGOProject/main.py
@@ -9,14 +9,13 @@ if __name__ == "__main__":
                         ]
     returned_data = data_cleaning.clean_data(filename=files_to_be_read[0], is_write=False)
     number = returned_data[0]["no_of_nodes"]
-    nodes = []
+    graph_nodes = []
     for i in range(1, number+1):
         n1 = nd.node(node_name=i-1, x_axis=returned_data[i][i-1][0],
-                        y_axis=returned_data[i][i-1][1], edge_cost=returned_data[i+number][i-1]
-                    )
-        nodes.append(n1)
+                        y_axis=returned_data[i][i-1][1], edge_cost=returned_data[i+number][i-1])
+        graph_nodes.append(n1)
     
-    for node in nodes:
+    for node in graph_nodes:
         print(node)
     # print(nodes[0])
     # print(nodes[0].get_cost())
